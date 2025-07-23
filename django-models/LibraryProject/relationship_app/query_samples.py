f5246783adf09ee97b1bf55241b7e308ba41637a
yirubal <yiruubal@gmail.com>
2025-07-23 23:38:02 +0300

    add models for library

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,18 @@
 from relationship_app.models import Author, Library
 
-# 1. Query all books by a specific author
-author = Author.objects.get(name="George Orwell")
+# --- 1. Query all books by a specific author ---
+author_name = "George Orwell"  # You can change this
+author = Author.objects.get(name=author_name)
 books_by_author = author.books.all()
-print("Books by George Orwell:", [book.title for book in books_by_author])
+print(f"Books by {author_name}:", [book.title for book in books_by_author])
 
-# 2. List all books in a library
-library = Library.objects.get(name="City Library")
+# --- 2. List all books in a specific library ---
+library_name = "City Library"  # Change this to test another library
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
-print("Books in City Library:", [book.title for book in books_in_library])
+print(f"Books in {library_name}:", [book.title for book in books_in_library])
 
-# 3. Retrieve the librarian for a library
-library = Library.objects.get(name="City Library")
-librarian = library.librarian  # thanks to related_name in OneToOneField
-print("Librarian for City Library:", librarian.name)
+# --- 3. Retrieve the librarian for a library ---
+librarian = library.librarian  # Uses related_name from OneToOneField
+print(f"Librarian for {library_name}:", librarian.name)
+
