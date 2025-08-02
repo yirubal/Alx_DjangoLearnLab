4e94de2733ec8a6bdd9f99044adba15201a59f01
yirubal <yiruubal@gmail.com>
2025-08-02 04:00:45 +0300

    add security feature

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,3 +2,8 @@ from django import forms
 
 class SearchForm(forms.Form):
     title = forms.CharField(max_length=100)
+
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(label='Name', max_length=100)
+    email = forms.EmailField(label='Email')
