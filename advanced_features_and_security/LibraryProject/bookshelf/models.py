a59c7c260c01c7e058b963b4f4bb1983ffcb8e2d
yirubal <yiruubal@gmail.com>
2025-08-02 03:25:48 +0300

    add custom authentication

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager, User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from django.conf import settings
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -9,4 +15,57 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+
+# User Roles
+ROLE_CHOICES = (
+    ('Admin', 'Admin'),
+    ('Librarian', 'Librarian'),
+    ('Member', 'Member'),
+)
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(username, email, password, **extra_fields)
+
+
+class UserProfile:
+    pass
+
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profiles/', null=True, blank=True)
+
+    objects = CustomUserManager()  # Attach the custom manager
+
+    def __str__(self):
+        return self.username
+
+    class UserProfile(models.Model):
+        user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+        role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+        def __str__(self):
+            return f"{self.user.username} - {self.role}"
+
+    # Automatically create profile when user is created
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.userprofile.save()
