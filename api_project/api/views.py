3e6c233dd0fba9fcc0e31313600c71dfe97c1984
yirubal <yiruubal@gmail.com>
2025-08-02 21:18:36 +0300

    add task 3

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-
+from rest_framework.permissions import IsAuthenticated
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
@@ -12,3 +12,4 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
