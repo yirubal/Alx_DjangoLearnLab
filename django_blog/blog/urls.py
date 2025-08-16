439d31656e683dfd4529b3824bef1b26b016e36f
yirubal <yiruubal@gmail.com>
2025-08-16 13:42:43 +0300

    update template URLs in `post_list.html` and `post_detail.html` to match the latest view names; update `db.sqlite3` after migrations

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,12 +16,14 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("profile/", ProfileView.as_view(), name="profile"),
 
-    # Posts
+
     path("posts/", PostListView.as_view(), name="posts"),                 # list
     path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"), # detail
-    path('post/new', PostCreateView.as_view(), name='post-form'), #new
-    path('post/<int:pk>/edit', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
+
+
+    path("post/new/", PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
 
 
 ]
