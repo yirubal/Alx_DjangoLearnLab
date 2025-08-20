27d8d213479e0bbc1a52738679f853204319b5f5
yirubal <yiruubal@gmail.com>
2025-08-20 10:28:38 +0300

    update `UserSerializer` to use `get_user_model` for flexibility; enhance password handling; adjust `bio` and `profile_pic` logic; ensure token creation during user creation

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,44 +1,40 @@
 from rest_framework import serializers
-from .models import CustomUser
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
-from .models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required = False)
-
+    # Specify the password field explicitly as CharField (since it's required)
+    password = serializers.CharField(write_only=True, required=True)
 
     class Meta:
-        model = CustomUser
-        fields = ('username', 'email', 'bio', 'profile_pic' 'password')
-
+        model = get_user_model()
+        fields = ('username', 'email', 'bio', 'profile_pic', 'password')
 
     def create(self, validated_data):
+        # Use get_user_model().objects.create_user for better compatibility
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        user.bio = validated_data.get('bio', '')
+        user.profile_pic = validated_data.get('profile_pic', None)
+        user.save()
 
-      user = get_user_model().objects.create(
-          username = validated_data['username'],
-          email = validated_data['email'],
-          password = validated_data['password']
-      )
-      user.bio = validated_data.get('bio', user.bio)
-      user.profile_picture = validated_data.get('profile_picture', user.profile_picture)
-      user.save()
-      Token.objects.create(user=user)
-      return user
-
-
-
+        # Create a token for the user
+        Token.objects.create(user=user)
+        return user
 
     def update(self, instance, validated_data):
-
         instance.username = validated_data.get('username', instance.username)
         instance.email = validated_data.get('email', instance.email)
         instance.bio = validated_data.get('bio', instance.bio)
         instance.profile_pic = validated_data.get('profile_pic', instance.profile_pic)
 
+        # Handle password update
         password = validated_data.get('password', None)
         if password:
             instance.set_password(password)
+
         instance.save()
         return instance
-
